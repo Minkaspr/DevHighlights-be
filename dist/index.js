5d69dea781945fa3e5ef3000f6a2c4f71c71e455
Dario Quispe <106852390+Minkaspr@users.noreply.github.com>
2025-03-04 19:19:47 -0500

    feat: implement CORS configuration with dynamic origin validation and credentials support

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const prisma_1 = require("./db-config/prisma");
 if (process.env.NODE_ENV !== "production") {
     process.loadEnvFile();
@@ -13,6 +14,21 @@ const routes_1 = __importDefault(require("./routes/routes"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 const AUTHOR = process.env.AUTHOR || "Desconocido";
+const allowedOrigins = [
+    "http://localhost:4200",
+    "https://portfolio-plum-iota-60.vercel.app"
+];
+app.use((0, cors_1.default)({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.json({ message: "API funcionando" });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { testConnection } from "./db-config/prisma";
 if (process.env.NODE_ENV !== "production") {
   process.loadEnvFile();
@@ -9,6 +10,22 @@ import router from "./routes/routes";
 const app = express();
 const PORT = process.env.PORT || 3000;
 const AUTHOR = process.env.AUTHOR || "Desconocido";
+
+const allowedOrigins = [
+  "http://localhost:4200", 
+  "https://portfolio-plum-iota-60.vercel.app"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, 
+}));
 
 app.use(express.json());
 
